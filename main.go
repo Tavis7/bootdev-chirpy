691822902754f27fcba688df7f7e632c4e1fdcdc
tavis <tavis@mostlyirrational.com>
2025-12-22 12:28:38 -0700

    Hit counter

--- main.go
+++ main.go
@@ -3,18 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	fmt.Println("Starting server")
 
+	cfg := &apiConfig{}
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/healthz", http.HandlerFunc(healthHandler))
-	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.Handle("/metrics", http.HandlerFunc(cfg.getStatsHandler))
+	serveMux.Handle("/reset", http.HandlerFunc(cfg.resetStatsHandler))
+	serveMux.Handle("/app/", cfg.middlewareMetricsInc(
+		http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
-	server := &http.Server {
+	server := &http.Server{
 		Handler: serveMux,
-		Addr: ":8080",
+		Addr:    ":8080",
 	}
 
 	err := server.ListenAndServe()
@@ -22,6 +31,26 @@ func main() {
 		fmt.Println("Error: %v", err)
 		return
 	}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getStatsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetStatsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte{})
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
