d6a766e3c769bed25b9d3302714342682b165df1
tavis <tavis@mostlyirrational.com>
2025-12-23 11:44:47 -0700

    /api/ and /admin/

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ func main() {
 
 	cfg := &apiConfig{}
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/healthz", http.HandlerFunc(healthHandler))
-	serveMux.Handle("/metrics", http.HandlerFunc(cfg.getStatsHandler))
-	serveMux.Handle("/reset", http.HandlerFunc(cfg.resetStatsHandler))
+	serveMux.Handle("GET /api/healthz", http.HandlerFunc(healthHandler))
+	serveMux.Handle("GET /admin/metrics", http.HandlerFunc(cfg.getStatsHandler))
+	serveMux.Handle("POST /admin/reset", http.HandlerFunc(cfg.resetStatsHandler))
 	serveMux.Handle("/app/", cfg.middlewareMetricsInc(
 		http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
@@ -41,9 +41,17 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) getStatsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
-	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf(
+		""+
+			"<html>\n"+
+			"    <body>\n"+
+			"        <h1>Welcome, Chirpy Admin</h1>\n"+
+			"        <p>Chirpy has been visited %v times!</p>\n"+
+			"    </body>\n"+
+			"</html>\n",
+		cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) resetStatsHandler(w http.ResponseWriter, r *http.Request) {
